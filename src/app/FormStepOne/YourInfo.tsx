0e22ee9528f29aca63ba27bc3e5297963de17327
Ignacio <ignaciosabiajes@gmail.com>
2023-08-24 11:12:13 -0300

    first commit

---
+++ src/app/FormStepOne/YourInfo.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './YourInfo.css'
+
+function YourInfo() {
+  return (
+    <>
+     <h1>Personal info</h1>
+     <p>Please provide your name, email adress, and phone number</p>
+     <label className='personalInfoLabel'>Name</label>
+     <div>
+        <input name='Name' placeholder='e.g Stephen King'/>
+     </div>
+     <label className='personalInfoLabel'>Email Adress</label>
+     <div>
+        <input name='email' placeholder='e.g stephenking@lorem.com'/>
+     </div>
+     <label className='personalInfoLabel'>Phone Number</label>
+     <div>
+        <input name='phone' placeholder='e.g +1 234 567 890'/>
+     </div>
+     
+     
+     
+    </>
+  )
+}
+
+export default YourInfo
