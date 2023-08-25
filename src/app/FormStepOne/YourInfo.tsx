7d2b5a89d6e753414ce9913cb5fc8f263571886d
Ignacio <ignaciosabiajes@gmail.com>
2023-08-25 11:13:26 -0300

    main fomr stylized

--- src/app/FormStepOne/YourInfo.tsx
+++ src/app/FormStepOne/YourInfo.tsx
@@ -20,7 +20,6 @@ function YourInfo() {
      </div>
      
      
-     
     </>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,33 @@ export default function Home() {
 
     <main className="mainForm">
      <aside className="formSteps">
-      <div>
-        <h4>STEP 1</h4>
-        <h1>YOUR INFO</h1>
+      <div className="formStep">
+        <button className="formStepButton">1</button>
+        <aside>
+          <h4>STEP 1</h4>
+          <h1>YOUR INFO</h1>
+        </aside> 
       </div>
-      <div>
-        <h4>STEP 2</h4>
-        <h1>SELECT PLAN</h1>
+      <div className="formStep">
+        <button className="formStepButton">2</button>
+        <aside>
+          <h4>STEP 2</h4>
+          <h1>SELECT PLAN</h1>
+        </aside>
       </div> 
-      <div>
-        <h4>STEP 3</h4>
-        <h1>ADD-ONS</h1>  
+      <div className="formStep">
+        <button className="formStepButton">3</button>
+        <aside>
+          <h4>STEP 3</h4>
+          <h1>ADD-ONS</h1>  
+        </aside> 
       </div> 
-      <div>
-        <h4>STEP 4</h4>
-        <h1>SUMMARY</h1>
+      <div className="formStep">
+        <button className="formStepButton">4</button>
+        <aside>
+          <h4>STEP 4</h4>
+          <h1>SUMMARY</h1>
+        </aside>  
       </div>
      </aside>
      <aside>
