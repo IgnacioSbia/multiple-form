dfcb9564091030b26a2def8fafb34711b70a82d4
Ignacio <ignaciosabiajes@gmail.com>
2023-08-27 11:22:46 -0300

    minor changes

--- src/app/FormStepOne/YourInfo.tsx
+++ src/app/FormStepOne/YourInfo.tsx
@@ -4,22 +4,23 @@ import './YourInfo.css'
 function YourInfo() {
   return (
     <>
-     <h1>Personal info</h1>
-     <p>Please provide your name, email adress, and phone number</p>
-     <label className='personalInfoLabel'>Name</label>
-     <div>
-        <input name='Name' placeholder='e.g Stephen King'/>
-     </div>
-     <label className='personalInfoLabel'>Email Adress</label>
-     <div>
-        <input name='email' placeholder='e.g stephenking@lorem.com'/>
-     </div>
-     <label className='personalInfoLabel'>Phone Number</label>
-     <div>
-        <input name='phone' placeholder='e.g +1 234 567 890'/>
-     </div>
+    <main className='yourInfo'>
+      <h1 className='yourInfoTitle'>Personal info</h1>
+      <p className='yourInfoSubTitle'>Please provide your name, email adress, and phone number</p>
+      <label className='personalInfoLabel'>Name</label>
+      <div>
+         <input name='Name' placeholder='e.g Stephen King' className='yourInfoInputs'/>
+      </div>
+      <label className='personalInfoLabel'>Email Adress</label>
+      <div>
+         <input name='email' placeholder='e.g stephenking@lorem.com' className='yourInfoInputs'/>
+      </div>
+      <label className='personalInfoLabel'>Phone Number</label>
+      <div>
+         <input name='phone' placeholder='e.g +1 234 567 890' className='yourInfoInputs'/>
+      </div>
      
-     
+     </main>
     </>
   )
 }
